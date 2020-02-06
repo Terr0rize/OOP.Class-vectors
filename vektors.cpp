b0dc2e1f8cceddd824055548fd7fe7a104d56cb4
Terr0rize <57214464+Terr0rize@users.noreply.github.com>
2020-02-06 10:57:12 +0400

    Update vektors.cpp

--- vektors.cpp
+++ vektors.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include <iostream>
 #include <math.h>
 #include <conio.h>
@@ -13,7 +12,7 @@ void vektors::sl(double x1, double y1, double x2, double y2)
 	double x, y;
 	x = x1 + x2;
 	y = y1 + y2;
-	cout << "Результат сложения: С(" << x << "," << y << ",)\n";
+	cout << "Результат сложения: С(" << x << "," << y << ")\n";
 };
 void vektors::vich(double x1, double y1, double x2, double y2)
 {
@@ -29,7 +28,7 @@ void vektors::proiz(double x1, double y1, double x2, double y2)
 	cout << "Скалярное произведение=" << ps << "\n";
 	pvx = y1 * x2 - x1 * y2;
 	pvy = x1 * y2 - y1 * x2;
-	cout << "Векторное произведение: С(" << pvx << "," << pvy << ",)\n";
+	cout << "Векторное произведение: С(" << pvx << "," << pvy << ")\n";
 };
 void vektors::dlin(double x1, double y1, double x2, double y2)
 {
@@ -44,7 +43,7 @@ void vektors::dlin(double x1, double y1, double x2, double y2)
 void vektors::cosin(double x1, double y1, double x2, double y2)
 {
 	double c, u;
-	c = (x1*x2 + y1 * y2) / (sqrt(pow(x1, 2) + pow(y2, 2)*sqrt(pow(x2, 2) + pow(y2, 2))));
+	c = (x1 * x2 + y1 * y2) / (sqrt(pow(x1, 2) + pow(y2, 2) * sqrt(pow(x2, 2) + pow(y2, 2))));
 	u = acos(c);
 	cout << "Cos a=" << setprecision(5) << c << '\n';
 };
